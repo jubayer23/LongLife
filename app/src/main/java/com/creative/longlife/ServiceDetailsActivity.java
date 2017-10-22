a4c911dc2cdd0d5129059cf9a9de08ce71b054b5
jubayer <jubayer@shaficonsultancy.com>
2017-10-22 18:09:02 +0600

    sadsdsa

--- app/src/main/java/com/creative/longlife/ServiceDetailsActivity.java
+++ app/src/main/java/com/creative/longlife/ServiceDetailsActivity.java
@@ -1,10 +1,15 @@
 package com.creative.longlife;
 
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.creative.longlife.adapter.ServiceListAdapter2;
 import com.creative.longlife.appdata.MydApplication;
@@ -33,7 +38,6 @@ public class ServiceDetailsActivity extends AppCompatActivity implements View.On
         setContentView(R.layout.activity_service_details);
 
         current_service = (Service) getIntent().getSerializableExtra(ServiceListAdapter2.KEY_SERVICE);
-        Log.d("DEBUG",current_service.getId());
 
         init();
     }
@@ -81,9 +85,32 @@ public class ServiceDetailsActivity extends AppCompatActivity implements View.On
         int id = v.getId();
 
         if (id == R.id.fab_call) {
+            Intent intent = new Intent(Intent.ACTION_DIAL);
+            intent.setData(Uri.parse("tel:" + current_service.getCompany().getPhone()));
+            startActivity(intent);
 
         }
         if (id == R.id.fab_chat) {
+
+            PackageManager pm=getPackageManager();
+            try {
+
+                Intent waIntent = new Intent(Intent.ACTION_SEND);
+                waIntent.setType("text/plain");
+                String text = "Type your message";
+
+                PackageInfo info=pm.getPackageInfo("com.whatsapp", PackageManager.GET_META_DATA);
+                //Check if package exists or not. If not then code
+                //in catch block will be called
+                waIntent.setPackage("com.whatsapp");
+
+                waIntent.putExtra(Intent.EXTRA_TEXT, text);
+                startActivity(Intent.createChooser(waIntent, "Share with"));
+
+            } catch (PackageManager.NameNotFoundException e) {
+                Toast.makeText(this, "WhatsApp not Installed", Toast.LENGTH_SHORT)
+                        .show();
+            }
 
         }
         if (id == R.id.fab_share_link) {
